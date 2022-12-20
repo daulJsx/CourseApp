9973da92cc5a82e051b8447ac2ba9a12848df27b
Zianasti <ziandwiasti23@gmail.com>
2022-12-20 20:49:05 +0700

    Update Activity

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -26,115 +26,19 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // creating a variables for our recycler view.
-    private RecyclerView coursesRV;
-    private static final int ADD_COURSE_REQUEST = 1;
-    private static final int EDIT_COURSE_REQUEST = 2;
-    private ViewModal viewmodal;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // initializing our variable for our recycler view and fab.
-        coursesRV = findViewById(R.id.idRVCourses);
-        FloatingActionButton fab = findViewById(R.id.idFABAdd);
-
-        // adding on click listener for floating action button.
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // starting a new activity for adding a new course
-                // and passing a constant value in it.
-                Intent intent = new Intent(MainActivity.this, activity_new_course.class);
-                startActivityForResult(intent, ADD_COURSE_REQUEST);
-            }
-        });
-
-        // setting layout manager to our adapter class.
-        coursesRV.setLayoutManager(new LinearLayoutManager(this));
-        coursesRV.setHasFixedSize(true);
-
-        // initializing adapter for recycler view.
-        final CourseRVAdapter adapter = new CourseRVAdapter();
-
-        // setting adapter class for recycler view.
-        coursesRV.setAdapter(adapter);
-
-        // passing a data from view modal.
-        viewmodal = ViewModelProviders.of(this).get(ViewModal.class);
-
-        // below line is use to get all the courses from view modal.
-        viewmodal.getAllCourses().observe(this, new Observer<List<CourseModal>>() {
-            @Override
-            public void onChanged(List<CourseModal> models) {
-                // when the data is changed in our models we are
-                // adding that list to our adapter class.
-                adapter.submitList(models);
-            }
-        });
-        // below method is use to add swipe to delete method for item of recycler view.
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                // on recycler view item swiped then we are deleting the item of our recycler view.
-                viewmodal.delete(adapter.getCourseAt(viewHolder.getAdapterPosition()));
-                Toast.makeText(MainActivity.this, "Course deleted", Toast.LENGTH_SHORT).show();
-            }
-        }).
-                // below line is use to attach this to recycler view.
-                        attachToRecyclerView(coursesRV);
-        // below line is use to set item click listener for our item of recycler view.
-        adapter.setOnItemClickListener(new CourseRVAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(CourseModal model) {
-                // after clicking on item of recycler view
-                // we are opening a new activity and passing
-                // a data to our activity.
-                Intent intent = new Intent(MainActivity.this, activity_new_course.class);
-                intent.putExtra(activity_new_course.EXTRA_ID, model.getId());
-                intent.putExtra(activity_new_course.EXTRA_COURSE_NAME, model.getCourseName());
-                intent.putExtra(activity_new_course.EXTRA_DESCRIPTION, model.getCourseDescription());
-                intent.putExtra(activity_new_course.EXTRA_DURATION, model.getCourseDuration());
-
-                // below line is to start a new activity and
-                // adding a edit course constant.
-                startActivityForResult(intent, EDIT_COURSE_REQUEST);
-            }
-        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ADD_COURSE_REQUEST && resultCode == RESULT_OK) {
-            String courseName = data.getStringExtra(activity_new_course.EXTRA_COURSE_NAME);
-            String courseDescription = data.getStringExtra(activity_new_course.EXTRA_DESCRIPTION);
-            String courseDuration = data.getStringExtra(activity_new_course.EXTRA_DURATION);
-            CourseModal model = new CourseModal(courseName, courseDescription, courseDuration);
-            viewmodal.insert(model);
-            Toast.makeText(this, "Course saved", Toast.LENGTH_SHORT).show();
-        } else if (requestCode == EDIT_COURSE_REQUEST && resultCode == RESULT_OK) {
-            int id = data.getIntExtra(activity_new_course.EXTRA_ID, -1);
-            if (id == -1) {
-                Toast.makeText(this, "Course can't be updated", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            String courseName = data.getStringExtra(activity_new_course.EXTRA_COURSE_NAME);
-            String courseDesc = data.getStringExtra(activity_new_course.EXTRA_DESCRIPTION);
-            String courseDuration = data.getStringExtra(activity_new_course.EXTRA_DURATION);
-            CourseModal model = new CourseModal(courseName, courseDesc, courseDuration);
-            model.setId(id);
-            viewmodal.update(model);
-            Toast.makeText(this, "Course updated", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Course not saved", Toast.LENGTH_SHORT).show();
-        }
+    public void CourseMenu(View v) {
+        Intent i = new Intent(this, ListDataCourse.class);
+        startActivity(i);
+    }
+
+    public void studentMenu(View v) {
+        Intent i = new Intent(this, ListDataStudent.class);
+        startActivity(i);
     }
 }
