480e935852b7bdd03d375b97a1d149b7517ee540
resa23 <laresarianti23@gmail.com>
2022-12-22 14:42:21 +0700

    xml activity course dan activity student

--- app/src/main/java/com/myapplication/activity_new_course.java
+++ app/src/main/java/com/myapplication/activity_new_course.java
@@ -25,7 +25,7 @@ public class activity_new_course extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_course);
+        setContentView(R.layout.activity_course_new);
 
         // initializing our variables for each view.
         courseNameEdt = findViewById(R.id.idEdtCourseName);
